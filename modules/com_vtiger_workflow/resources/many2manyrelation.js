c9e4fdc185991db096b8cc9d03bc4bcad967f4e5
Rodgers <rd_sande@yahoo.com>
2020-07-15 19:09:10 +0300

    Converting the many2manyrelation file to LDS

--- modules/com_vtiger_workflow/resources/many2manyrelation.js
+++ modules/com_vtiger_workflow/resources/many2manyrelation.js
@@ -41,22 +41,24 @@ function doGlobalGridSelect() {
 }
 
 function addrecList(recId, recvalue) {
-	var reclist ='<tr id="row-'+recId+'" aria-level="1" aria-posinset="1" aria-selected="false" aria-setsize="4">'+
-		'<td class="slds-text-align_right" role="gridcell" style="width: 3.25rem;">'+
-		'<div class="slds-checkbox">'+
-			'<input type="checkbox" onclick="changeIdlistVal('+recId+')" name="options[]" value='+recId+' id="checkbox-'+recId+'" aria-labelledby="check-button-label-04 column-group-header" value="checkbox-04" checked />'+
-			'<label class="slds-checkbox__label" for="checkbox-'+recId+'" id="check-button-label-04">'+
-			'<span class="slds-checkbox_faux"></span>'+
-			'<span class="slds-form-element__label slds-assistive-text">Select item 4</span>'+
-			'</label>'+
-		'</div>'+
-		'</td>'+
-		'<td class="slds-tree__item" data-label="Entity Name" scope="row">'+
-			'<div class="slds-truncate">'+(document.getElementById('radio-5').checked ? '*' : recvalue)+'</div>'+
-		'</td>'+
-		'<td data-label="Entity" role="gridcell" style="width: 18rem;">'+
-			'<div class="slds-truncate">'+document.getElementById('relModlist_type').value+'</div>'+
-		'</td>';
+	var reclist =
+		`<div class="slds-grid" id="row-${recId}">
+			<div class="slds-col slds-size_1-of-12 slds-p-around_x-small">
+				<div class="slds-checkbox">
+					<input type="checkbox" onclick="changeIdlistVal(${recId})" name="options[]" value=${recId} id="checkbox-${recId}" aria-labelledby="check-button-label-04 column-group-header" value="checkbox-04" checked />
+						<label class="slds-checkbox__label" for="checkbox-${recId}" id="check-button-label-04">
+						<span class="slds-checkbox_faux"></span>
+						<span class="slds-form-element__label slds-assistive-text">Select item 4</span>
+						</label>
+				</div>
+			</div>
+			<div class="slds-col slds-size_4-of-12 slds-p-around_x-small">
+				<div class="slds-truncate">${(document.getElementById('radio-5').checked ? '*' : recvalue)}</div>
+			</div>
+			<div class="slds-col slds-size_4-of-12 slds-p-around_x-small">
+				<div class="slds-truncate slds-float_right">${document.getElementById('relModlist_type').value}</div>
+			</div>
+		</div>`;
 	return reclist;
 }
 
@@ -64,22 +66,24 @@ jQuery(document).ready(function () {
 	var recsavedDiv='';
 	if (relrecords.length > 0) {
 		for (var i=0; i<relrecords.length; i++) {
-			var reclist ='<tr id="row-'+relrecords[i].recid+'" aria-level="1" aria-posinset="1" aria-selected="false" aria-setsize="4">'+
-				'<td class="slds-text-align_right" role="gridcell" style="width: 3.25rem;">'+
-				'<div class="slds-checkbox">'+
-					'<input type="checkbox" onclick="changeIdlistVal('+relrecords[i].recid+')" name="options[]" value='+relrecords[i].recid+' id="checkbox-'+relrecords[i].recid+'" aria-labelledby="check-button-label-04 column-group-header" value="checkbox-04" checked />'+
-					'<label class="slds-checkbox__label" for="checkbox-'+relrecords[i].recid+'" id="check-button-label-04">'+
-					'<span class="slds-checkbox_faux"></span>'+
-					'<span class="slds-form-element__label slds-assistive-text">Select item 4</span>'+
-					'</label>'+
-				'</div>'+
-				'</td>'+
-				'<td class="slds-tree__item" data-label="Entity Name" scope="row">'+
-					'<div class="slds-truncate">'+(document.getElementById('radio-5').checked ? '*' : relrecords[i].entityName)+'</div>'+
-				'</td>'+
-				'<td data-label="Entity" role="gridcell" style="width: 18rem;">'+
-					'<div class="slds-truncate">'+relrecords[i].entityType+'</div>'+
-				'</td>';
+			var reclist =`
+			<div class="slds-grid slds-p-horizontal_x-large" id="row-${relrecords[i].recid}">
+				<div class="slds-col slds-size_1-of-12 slds-p-around_x-small">
+					<div class="slds-checkbox">
+						<input type="checkbox" onclick="changeIdlistVal(${relrecords[i].recid})" name="options[]" value=${relrecords[i].recid} id="checkbox-${relrecords[i].recid}" aria-labelledby="check-button-label-04 column-group-header" value="checkbox-04" checked />
+						<label class="slds-checkbox__label" for="checkbox-${relrecords[i].recid}" id="check-button-label-04">
+						<span class="slds-checkbox_faux"></span>
+						<span class="slds-form-element__label slds-assistive-text">Select item 4</span>
+						</label>
+					</div>
+				</div>
+				<div class="slds-col slds-size_4-of-12 slds-p-around_x-small" data-label="Entity Name">
+					<div class="slds-truncate">${(document.getElementById('radio-5').checked ? '*' : relrecords[i].entityName)}</div>
+				</div>
+				<div class="slds-col slds-size_4-of-12 slds-p-around_x-small" data-label="Entity">
+					<div class="slds-truncate slds-float_right">${relrecords[i].entityType}</div>
+				</div>
+			</div>`;
 			recsavedDiv += reclist;
 		}
 		document.getElementById('selected_recordsDiv').innerHTML = recsavedDiv;
